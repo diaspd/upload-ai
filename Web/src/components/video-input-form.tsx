3471d1a37992a03a98ae0be651fa01bc48406a93
diaspd <pdr050203@gmail.com>
2023-09-17 11:31:21 -0300

    added converter from mp4 to mp3

--- Web/src/components/video-input-form.tsx
+++ Web/src/components/video-input-form.tsx
@@ -3,10 +3,13 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInputForm() { 
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -20,8 +23,53 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+  async function convertVideoToAudio(video: File) {
+    console.log('Converting...')
 
+    const ffmpeg = await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+    const adioFile = new File([audioFileBlob], 'audio.mp3', {
+      type: 'audio/mpeg',
+    })
+
+    console.log('Conver finished.')
+
+    return adioFile
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) {
+      return
+    }
+
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    console.log(audioFile)
   }
 
   const previewURL = useMemo(() => {
@@ -57,6 +105,7 @@ export function VideoInputForm() {
         Prompt de transcrição
       </Label>
       <Textarea
+        ref={promptInputRef}
         id="transcription_prompt" 
         className="h-20 leading-relaxed resize-none" 
         placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: { 
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
