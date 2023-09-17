4d1a04deac12621c3c0be1c9df9b7eebf03c3f60
diaspd <pdr050203@gmail.com>
2023-09-17 13:47:54 -0300

    added prompt via back-end with seeds

--- API/src/routes/generate-ai-completion.ts
+++ API/src/routes/generate-ai-completion.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { streamToResponse, OpenAIStream } from 'ai'
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -30,9 +31,18 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage }
-      ]
+      ],
+      stream: true,
+
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    })
   })
 }
